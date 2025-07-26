e5825710da93fd9ef99f099f85c981d40dec1af4
Hooninator <jbellavita@berkeley.edu>
2025-07-26 13:32:33 -0400

    more python updates

--- ptmc/ttmc.py
+++ ptmc/ttmc.py
@@ -19,6 +19,14 @@ base = "../tensors/"
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+timers = {"tensor_norm_time":[],
+          "matrix_norm_time":[],
+          "tensor_recover_time":[],
+          "ttm_time":[],
+          "ttmc_baseline_time":[],
+          "ttmc_mixed_time":[]}
+
+
 precisions = { "fp16":torch.float16,
                "fp32":torch.float32,
                "fp64":torch.float64}
@@ -84,12 +92,18 @@ def init_matrices_unif(rows, cols, alpha, out):
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+def ordering_compute(rows, cols):
+    N = len(rows)
+    pi = np.zeros(N)
+    for i in range(N):
+        pi[i] = (1 / cols[i]) - (1/rows[i])
+    ordering = list(range(N))
+    ordering_best = [x for _, x in sorted(zip(pi, ordering), key=lambda p: p[0])]
+    print(ordering_best)
+    print(pi)
+    return ordering_best
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#               Normalization
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -98,26 +112,62 @@ def init_matrices_unif(rows, cols, alpha, out):
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-def ttmc_mixed(X, U_list, normalizer, ordering):
+def ttmc_mixed(X, U_list, normalizer, ordering, trial):
+
+    tensor_norm_time = 0
+    matrix_norm_time = 0
+    ttm_time = 0
+    tensor_recover_time = 0
 
     N = len(U_list)
 
     Y = X
+
     for n in range(N):
-        print(f"TTMc iteration {n}")
 
         mode = ordering[n]
 
+        # Normalize operands
+        t1 = time.time()
         Y = normalizer.normalize_tensor(Y, mode)
+        tensor_norm_time += (time.time() - t1)
+
+        t2 = time.time()
         U = normalizer.normalize_matrix(U_list[mode], mode)
+        matrix_norm_time += (time.time() - t2)
 
+        # Perform TTM
+        t3 = time.time()
         Y = tl.tenalg.mode_dot(Y, U, mode)
+        ttm_time += (time.time() - t3)
 
-        print(torch.max(Y))
-        print(Y.dtype)
+        # Recover output
+        t4 = time.time()
         Y = normalizer.recover_tensor(Y, mode)
+        tensor_recover_time += (time.time() - t4)
+
+    if trial > 0:
+        timers["tensor_norm_time"].append(tensor_norm_time)
+        timers["matrix_norm_time"].append(matrix_norm_time)
+        timers["ttm_time"].append(ttm_time)
+        timers["tensor_recover_time"].append(tensor_recover_time)
 
     return Y
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#              Error Measurements 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def frob_norm_err(Y_correct, Y_computed):
+    return (torch.norm(Y_correct - Y_computed)) / torch.norm(Y_correct)
+
+
+def componentwise_err(Y_correct, Y_computed):
+    err = torch.abs((Y_correct - Y_computed)) 
+    return torch.max(err)
+
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -137,10 +187,16 @@ def arg_to_str(args):
     return
 
 
+def print_times():
+    for timer in timers:
+        print(f"[{timer}]: {sum(timers[timer])}s")
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--tensor", type=str)
+    parser.add_argument("--ordering", type=str)
     parser.add_argument("--trials", type=int, default=10)
     parser.add_argument("--mat_rows", type=int, nargs='+')
     parser.add_argument("--mat_init", type=str)
@@ -170,28 +226,38 @@ if __name__ == "__main__":
 
     # Run TTMc
     for t in range(args.trials):
-        print(f"TRIAL {t}")
+
+        print(f"\tTRIAL {t}")
+
+        ttmc_mixed_time = 0
+        ttmc_baseline_time = 0
 
         # Determine ordering
+        #ordering = ordering_compute(args.mat_rows, X.shape)
         ordering = list(range(X.ndim))
-        random.shuffle(ordering)
+        #random.shuffle(ordering)
 
-        stime = time.time()
-        Y = ttmc_mixed(X, U_list, normalizer, ordering)
-        etime = time.time()
-        print(f"Time for mixed: {etime-stime}")
+        t9 = time.time()
+        Y_correct = tl.tenalg.multi_mode_dot(X, U_list)
+        ttmc_baseline_time = (time.time() - t9)
+
+        t0 = time.time()
+        Y = ttmc_mixed(X, U_list, normalizer, ordering, t)
+        ttmc_mixed_time = (time.time() - t0)
+
+        if t > 0:
+            timers["ttmc_mixed_time"].append(ttmc_mixed_time)
+            timers["ttmc_baseline_time"].append(ttmc_baseline_time)
 
         if args.correctness:
-            stime = time.time()
-            Y_correct = tl.tenalg.multi_mode_dot(X, U_list)
-            etime = time.time()
-            print(f"Time for normal: {etime-stime}")
-            err = torch.norm(Y - Y_correct) / torch.norm(Y_correct)
-            print(f"Err: {err}")
+            f_err = frob_norm_err(Y_correct, Y)
+            c_err = componentwise_err(Y_correct, Y)
+            print(f"||Y_hat - Y||F / ||Y_hat||F: {f_err}")
+            print(f"max|Y_hat - Y|: {c_err}")
 
     # Forward Error
 
-
+    print_times()
     print("~"*100)
 
 
